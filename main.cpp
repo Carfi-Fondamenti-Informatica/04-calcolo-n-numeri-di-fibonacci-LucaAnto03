da89c009049a7d37456d6c662ec37d607c908f60
Luca Antonelli <116542318+LucaAnto03@users.noreply.github.com>
2022-11-21 22:46:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, i=0, primo=1, secondo=0, terzo=0;
+    cin >> n ;
+    if (n>=2) {
+        for (i; i<n; i++){
+            terzo=primo+secondo;
+            primo=secondo;
+            secondo=terzo;
+            cout << terzo << endl;
+        }
+
+    } else {
+        cout << "errore" << endl;
+    }
+    return 0;
 }
